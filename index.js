2efb007e0668a2d0f3a8bff7c1dfe46eed93b8b3
Dharanisri <dharanishrids23106@gmail.com>
2024-11-10 00:47:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+let all = document.querySelectorAll("main div");
+let dots = document.querySelectorAll("span.dot");
+
+document.querySelector(".toogle").addEventListener("click", () => {
+    document.querySelector(".alert").innerHTML = "0";
+    for (a of all) {
+        a.style.backgroundColor = "white";
+        a.querySelector("div p span.dot").style.display = "none";
+    }
+});
+
+function changeNum() {
+    if (document.querySelector(".alert").innerHTML > 0) {
+        document.querySelector(".alert").innerHTML--;
+    } else {
+        document.querySelector(".alert").innerHTML = 0;
+    }
+}
+
+function bgChange(b) {
+    document.getElementById(b).style.backgroundColor = "white";
+}
+
+function displayChange(c) {
+    document.querySelector(c).style.display = "none";
+}
+
+for (a of all) {
+    let didi = a.id;
+    a.addEventListener("click", () => {
+        bgChange(didi);
+        displayChange("#" + didi + " div p span.dot");
+        changeNum();
+    }, { once: true });
+}
+
